bb9d5cbb75755a9fca156bedb41bd98a7a8852ee
kwak9898 <ktm9837@naver.com>
2023-04-13 13:36:14 +0900

    영화 전체 조회

--- src/exception/error-code.ts
+++ src/exception/error-code.ts
@@ -1,6 +1,7 @@
 export const MOVIE_EXCEPTION = {
   MOVIE_NOT_FOUND: {
     code: 'MOVIE_NOT_FOUND',
+    status: 404,
     message: '존재하는 영화가 없습니다.',
   },
 };
--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MovieRepository } from './movie.repository';
+import { MyPaginationQuery } from 'src/base/pagination-query';
+import { Movie } from './entities/movie.entity';
+import { Pagination, paginate } from 'nestjs-typeorm-paginate';
+import { MOVIE_EXCEPTION } from 'src/exception/error-code';
+import { MovieDto } from './dto/movie.dto';
 
 @Injectable()
-export class MovieService {}
+export class MovieService {
+  constructor(
+    @InjectRepository(MovieRepository)
+    private readonly movieRepository: MovieRepository,
+  ) {}
+
+  /**
+   * 전체 조회
+   *
+   * @param options query string parameter
+   * @returns
+   */
+  async getAllMovie(options: MyPaginationQuery): Promise<Pagination<Movie>> {
+    if (paginate(await this.movieRepository, options) === null) {
+      throw new NotFoundException(MOVIE_EXCEPTION.MOVIE_NOT_FOUND);
+    }
+    return paginate(await this.movieRepository, options);
+  }
+
+  async createMovie(createMovieDto: MovieDto): Promise<Movie> {
+    return this.movieRepository.createMovie(createMovieDto);
+  }
+}
