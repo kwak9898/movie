be0afe37d7595a7beb60d148fb4abb553a404fd3
kwak9898 <ktm9837@naver.com>
2023-04-16 14:30:51 +0900

    movie controller

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { MovieService } from './movie.service';
+import { MyPagination } from 'src/base/pagination-resoponse';
+import { MyPaginationQuery } from 'src/base/pagination-query';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { Movie } from './entities/movie.entity';
+import { MovieDto } from './dto/movie.dto';
 
 @Controller('movie')
-export class MovieController {}
+export class MovieController {
+  constructor(private movieService: MovieService) {}
+
+  @Get('')
+  async getAllByMovie(
+    @Query() query: MyPaginationQuery,
+  ): Promise<Pagination<Movie>> {
+    return await this.movieService.getAllMovie(query);
+  }
+
+  @Get(':movieId')
+  async findOneByMovie(@Param('movieId') movieId: number): Promise<Movie> {
+    return await this.findOneByMovie(movieId);
+  }
+
+  @Post('')
+  async createMovie(@Body() createMovie: MovieDto): Promise<Movie> {
+    return await this.createMovie(createMovie);
+  }
+
+  @Put(':movieId')
+  async updateMovie(
+    @Param(':movieId') movieId: number,
+    changeMovieDto: MovieDto,
+  ) {
+    return await this.updateMovie(movieId, changeMovieDto);
+  }
+
+  @Delete('movieId')
+  async deleteMovie(@Param(':movieId') movieId: number) {
+    return await this.deleteMovie(movieId);
+  }
+}
