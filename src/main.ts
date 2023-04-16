aedd89fff836a6886844528096fb515d6ae4720f
kwak9898 <ktm9837@naver.com>
2023-04-16 14:49:07 +0900

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  useSwagger(app);
   await app.listen(3000);
+
+  function useSwagger(app: INestApplication): void {
+    const config = new DocumentBuilder()
+      .setTitle('Pumpkin NestJS API')
+      .setDescription('API description')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('api', app, document);
+    console.log('APP START IN ' + process.env.NODE_ENV);
+  }
 }
+
 bootstrap();
