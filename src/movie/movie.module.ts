30d1b338a01b6dabc61e7c12281ad2627b9bbcbc
kwak9898 <ktm9837@naver.com>
2023-04-16 14:50:48 +0900

    ADD MovieRepository

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { MovieController } from './movie.controller';
+import { MovieRepository } from './movie.repository';
 
 @Module({
-  providers: [MovieService],
-  controllers: [MovieController]
+  providers: [MovieService, MovieRepository],
+  controllers: [MovieController],
+  exports: [MovieService, MovieRepository],
 })
 export class MovieModule {}
