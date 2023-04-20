9c2c0be2c018f27f3acc73824f82c98db8d72e1f
kwak9898 <ktm9837@naver.com>
2023-04-20 15:12:29 +0900

    create 관련 excpetion 추가

--- src/exception/error-code.ts
+++ src/exception/error-code.ts
@@ -15,4 +15,10 @@ export const MOVIE_EXCEPTION = {
     status: 400,
     message: '이미 존재하는 영화입니다.',
   },
+
+  MOVIE_NOT_CREATE: {
+    code: 'MOVIE_NOT_CREATE',
+    status: 400,
+    message: '생성할 영화 정보들을 적어주세요.',
+  },
 };
